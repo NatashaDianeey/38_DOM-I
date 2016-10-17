96bf1c62dcceff8a13293f001ff7b8b942f802d2
NatashaDianeey <ndianeey@gamil.com>
2016-10-17 13:31:31 +0000

    traductor

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -1,3 +1,9 @@
 function validateForm(){
 	/* Escribe tú código aquí */
 }
+
+function limitar(input-password) {
+         if (input-password.value.length > 50) {
+            return false
+            }	 
+         }
--- traduccion-formulario/js/main.js
+++ traduccion-formulario/js/main.js
@@ -1,7 +1,54 @@
-var idioma = " ";
+// var idioma = " ";
 
-if (idioma == "es") {
-	el usuario.innerHTML="usuario";
-	el pasport.innerHTML="Contraseña";
+// if (idioma == "es") {
+// 	el usuario.innerHTML="usuario";
+// 	el pasport.innerHTML="Contraseña";
 	
+// }
+
+
+function cambiarIdioma (idioma) {
+	var elHead = document.getElementById('form-signin-heading');
+	var elEmail = document.getElementById('inputEmail');
+	var elPassword=document.getElementById('inputPassword');
+	var elRemember = document.getElementById('remember');
+	var elButton = document.getElementById('buttonSign');
+//Español
+
+//
+
+	elEmail.setAttribute('placeholder','Please');
+	var input_email = document.getElementById('Correo');
+
+	elPassword.setAttribute('placeholder', 'Password');
+	var input_email = document.getElementById('Constraseña');
+
+	
+
+	if (idioma == "es") {
+		elHead.innerHTML="Por favor inicia sesion";
+		elEmail.innerHTML="Correo";
+		elPassword.innerHTML="Contraseña";
+		elRemember.innerHTML="Recordar datos"
+		elButton.innerHTML="Iniciar Sesión"
+
+		elContra.setAttribute('placeholder','Ingresa Contraseña');
+		elCorreo.setAttribute('placeholder', 'Ingresa Correo')
+	} else {
+		elHead.innerHTML='form-signin-heading';
+		elEmail.innerHTML ='inputEmail';
+		elPassword.innerHTML ='inputPassword';
+		elContra.setAttribute('placeholder','inputPassword');
+		elCorreo.setAttribute('placeholder','inputEmail');
+	}
+} 
+
+var btnEs = document.getElementById('btnEs');
+btnEs.onclick = function () {
+	cambiarIdioma ("es");
+}
+
+var btnEn = document.getElementById('btnEn');
+btnEn.onclick = function () {
+	cambiarIdioma ("en")
 }
