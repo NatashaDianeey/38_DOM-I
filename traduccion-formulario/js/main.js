22cdd0f9a58f4ae060226556edebcd24f397a906
NatashaDianeey <ndianeey@gamil.com>
2016-10-17 16:38:15 +0000

    Ej Traductor terminado

--- traduccion-formulario/js/main.js
+++ traduccion-formulario/js/main.js
@@ -1,11 +1,3 @@
-// var idioma = " ";
-
-// if (idioma == "es") {
-// 	el usuario.innerHTML="usuario";
-// 	el pasport.innerHTML="Contraseña";
-	
-// }
-
 
 function cambiarIdioma (idioma) {
 	var elHead = document.getElementById('form-signin-heading');
@@ -17,29 +9,21 @@ function cambiarIdioma (idioma) {
 
 //
 
-	elEmail.setAttribute('placeholder','Please');
-	var input_email = document.getElementById('Correo');
-
-	elPassword.setAttribute('placeholder', 'Password');
-	var input_email = document.getElementById('Constraseña');
-
-	
 
 	if (idioma == "es") {
 		elHead.innerHTML="Por favor inicia sesion";
-		elEmail.innerHTML="Correo";
-		elPassword.innerHTML="Contraseña";
-		elRemember.innerHTML="Recordar datos"
-		elButton.innerHTML="Iniciar Sesión"
+		elEmail.setAttribute('placeholder','Correo Electrónico');
+		elPassword.setAttribute('placeholder', 'Contraseña');
+		elRemember.innerHTML="Recordar datos";
+		elButton.innerHTML="Iniciar Sesión";
 
-		elContra.setAttribute('placeholder','Ingresa Contraseña');
-		elCorreo.setAttribute('placeholder', 'Ingresa Correo')
+		
 	} else {
-		elHead.innerHTML='form-signin-heading';
-		elEmail.innerHTML ='inputEmail';
-		elPassword.innerHTML ='inputPassword';
-		elContra.setAttribute('placeholder','inputPassword');
-		elCorreo.setAttribute('placeholder','inputEmail');
+		elHead.innerHTML="Please sign in";
+		elRemember.innerHTML="Remember me";
+		elButton.innerHTML="Sign in";
+		elPassword.setAttribute('placeholder','Password');
+		elEmail.setAttribute('placeholder','Please enter your email');
 	}
 } 
 
@@ -48,7 +32,7 @@ btnEs.onclick = function () {
 	cambiarIdioma ("es");
 }
 
-var btnEn = document.getElementById('btnEn');
+var btnEs = document.getElementById('btnEn');
 btnEn.onclick = function () {
-	cambiarIdioma ("en")
+	cambiarIdioma ("en");
 }
