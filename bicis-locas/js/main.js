87c18fd9b7fd6d496696a178ec4d4b0e4e4d2f96
NatashaDianeey <ndianeey@gamil.com>
2016-10-18 15:44:10 +0000

    Ejercicio 1 y 2 terminados

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -1,9 +1,36 @@
 function validateForm(){
 	/* Escribe tú código aquí */
-}
+	var nombre = document.getElementById('name').value;
+	var apellido = document.getElementById('lastname').value;
+	var contrasena = document.getElementById('input-password').value;
+	var correo = document.getElementById('input-email').value;
+	var seleccionar = document.querySelector("#selectoption").value;
 
-function limitar(input-password) {
-         if (input-password.value.length > 50) {
-            return false
-            }	 
-         }
+	if(nombre === "" || apellido === "" || correo === "" || contrasena === "") {
+		alert('Falta llenar algun campo');
+		return false;
+
+	} else if (!/^[A-Z][a-z]*$/g.test(document.getElementById('name').value) ){
+		alert('Unicamente la primera letra debe ser Mayuscula');
+		return false;
+
+	} else if (!/^[A-Z][a-z]*$/g.test(document.getElementById('lastname').value) ){
+		alert('Unicamente la primera letra debe ser Mayuscula');
+		return false;
+
+	}else if (contrasena.length<6) {
+		alert('La contrasena es muy corta');
+		return false;
+
+	} else if (contrasena === 'contraseña' || contrasena == 123456 || contrasena == 0987654) {
+		alert('Esta contraseña no es permitida');
+		return false;
+	
+	} else if (seleccionar == 0) {
+		alert("Elige tu tipo de bici.");
+
+	} else if (seleccionar == 0) {
+		alert("Elige tu tipo de bici.");
+	}
+	
+}
